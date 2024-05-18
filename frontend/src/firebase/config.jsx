8d1c1aff9e4ac1ff8bac2a87eca90bbb7fa52246
Aman Singh <amansingh20010801@gmail.com>
2024-05-18 16:20:59 +0530

    minor changes

--- frontend/src/firebase/config.jsx
+++ frontend/src/firebase/config.jsx
@@ -5,20 +5,14 @@ import "firebase/storage";
 // require('dotenv').config();
 // require
 const firebaseConfig = {
-  // apiKey:process.env.REACT_APP_apiKey,
-  // authDomain:process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
-  // measurementId: process.env.REACT_APP_measurementId
-  apiKey: "AIzaSyAm9UPvdRo06jGyOdRmlMH15wWZFGZkM1w",
-  authDomain: "hpdsx-62c05.firebaseapp.com",
-  projectId: "hpdsx-62c05",
-  storageBucket: "hpdsx-62c05.appspot.com",
-  messagingSenderId: "180990730647",
-  appId: "1:180990730647:web:e595dba6fd030dd00c24d2",
-  measurementId: "G-147779BRZP",
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
